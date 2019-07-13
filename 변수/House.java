68aa8ce620e3a49019c085e27d5bd4990cbf7dae
ecliarshirley <52848111+ecliarshirley@users.noreply.github.com>
2019-07-13 14:44:38 +0900

    Add files via upload

---
+++ 변수/House.java
@@ -0,0 +1,31 @@
+package 변수;
+
+public class House {
+
+	public static void main(String[] args) {
+		String roof = "green";
+		String phone = "8441234";
+		int room = 302;
+		int floor = 10;
+		double window = 50.5;
+		String location = "left";
+		boolean cool = true;
+		
+		System.out.println("---------------------------");
+		System.out.println("저의 집에 대한 정보입니다.");
+		System.out.println("---------------------------");
+		
+		System.out.println("집 지붕 색깔은 : " + roof);
+		System.out.println("집 전화 번호: " + phone);
+		System.out.println("호실: " + room);
+		System.out.println("층수: " + floor);
+		System.out.println("창문크기: " + window);
+		System.out.println("집 위치: " + location);
+		System.out.println("집이 시원합니까: " + cool);
+		
+		System.out.println("---------------------------");
+		
+		
+	}
+
+}
