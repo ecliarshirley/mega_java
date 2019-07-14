e7e8ab6234a1ee5643061b7ffeb64d94e660ed1c
shirleyshallwe <52848111+shirleyshallwe@users.noreply.github.com>
2019-07-14 16:39:58 +0900

    Add files via upload

--- 조건문/그래픽이미지넣기.java
+++ 조건문/그래픽이미지넣기.java
@@ -14,68 +14,77 @@ public class 그래픽이미지넣기 {
 		JFrame f = new JFrame();
 		f.setSize(800, 800);
 		
-		JButton btnNewButton = new JButton("\uBC14\uC704");
+		JButton btm = new JButton("\uACB0\uACFC\uAC12\uC774 \uC5EC\uAE30 \uD45C\uAE30\uB429\uB2C8\uB2E4");
+		f.getContentPane().add(btm, BorderLayout.SOUTH);
+		
+		
+		JButton top = new JButton("\uAC00\uC704\uBC14\uC774\uBCF4 \uAC8C\uC784");
+		f.getContentPane().add(top, BorderLayout.NORTH);
+		
+		JButton btnNewButton = new JButton("");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				System.out.println("바위를 누르셨군요");
+				System.out.println("가위 눌렀어요");
 				Random r = new Random();
-				//0 = 가위 , 1 = 바위, 2 = 보
+				
 				int computer = r.nextInt(3);
-				if (computer ==0) { //가위
-					System.out.println("이겼네요");
-				} else if(computer ==1 ) { //바위
-					System.out.println("비겼네요");
+				// 0 - 가위 1 - 보자기 2 - 주먹
+				if (computer == 0) {
+					btm.setText("비겼어요");
+				} else if(computer == 1) {
+					btm.setText("이겼네요!");
 				} else {
-					System.out.println("컴퓨터 승");
+					btm.setText("안타갑게 컴퓨터가 이겼어요");
 				}
+				
 			}
 		});
-		btnNewButton.setIcon(new ImageIcon("C:\\Users\\tjsc2-202-01\\Desktop\\\uBC14\uC7042.jpg"));
-		f.getContentPane().add(btnNewButton, BorderLayout.CENTER);
+		btnNewButton.setIcon(new ImageIcon("D:\\shirley\\day02\\\uAC00\uC704.jpg"));
+		f.getContentPane().add(btnNewButton, BorderLayout.WEST);
 		
-		JButton btnNewButton_1 = new JButton("\uAC00\uC704");
-		btnNewButton_1.setIcon(new ImageIcon("C:\\Users\\tjsc2-202-01\\Desktop\\\uAC00\uC704.jpg"));
+		JButton btnNewButton_1 = new JButton("");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				System.out.println("가위를 누르셨네요");
+				System.out.println("보자기를 눌렀어요");
 				Random r = new Random();
+				
 				int computer = r.nextInt(3);
-				//0 - 가위  / 1- 바위 / 2 - 보 
-				if (computer==0) {
-					System.out.println("비겼네요");
-				} else if (computer==1){
-					System.out.println("컴퓨터가 이겼네요");
-				}
-				else {
-					System.out.println("이겼네요");
+				// 0 - 가위 1 - 보자기 2 - 주먹
+				if (computer == 0) {
+					btm.setText("안타갑게 컴퓨터가 이겼어요");
+				} else if(computer == 1) {
+					btm.setText("비겼어요!");
+				} else {
+					btm.setText("이겼어요");
 				}
 				
 				
 			}
 		});
-		f.getContentPane().add(btnNewButton_1, BorderLayout.WEST);
+		btnNewButton_1.setIcon(new ImageIcon("D:\\shirley\\day02\\\uBC14\uC7042.jpg"));
+		f.getContentPane().add(btnNewButton_1, BorderLayout.CENTER);
 		
-		JButton btnNewButton_2 = new JButton("\uBCF4");
+		JButton btnNewButton_2 = new JButton("");
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				System.out.println("보자기 를 누르셨네요");
+				System.out.println("주먹을 눌렀어요");
 				Random r = new Random();
-				int computer = r.nextInt(2);
 				
-				if (computer ==0) {
-					System.out.println("이겼네요");
-					
-				} else if(computer ==1){
-					System.out.println("컴퓨터가 이겼어요");
+				int computer = r.nextInt(3);
+				// 0 - 가위 1 - 보자기 2 - 주먹
+				if (computer == 0) {
+					btm.setText("이겼어요");
+				} else if(computer == 1) {
+					btm.setText("안타갑게 컴퓨터가 이겼어요!");
 				} else {
-					System.out.println("비겼어요");
+					btm.setText("비겼네요");
 				}
-				
 				
 			}
 		});
-		btnNewButton_2.setIcon(new ImageIcon("C:\\Users\\tjsc2-202-01\\Desktop\\\uC8FC\uBA39.jpg"));
+		btnNewButton_2.setIcon(new ImageIcon("D:\\shirley\\day02\\\uC8FC\uBA39.jpg"));
 		f.getContentPane().add(btnNewButton_2, BorderLayout.EAST);
+		
 		
 		
 		f.setVisible(true);
